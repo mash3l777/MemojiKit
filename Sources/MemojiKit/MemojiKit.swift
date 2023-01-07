ba00abad6e1cc91e4289fc0652484f0014d3ba62
Bastien LE CORRE <bastien.lecorre@gmail.com>
2023-01-07 14:59:39 +0100

    feature(develop): background color

--- Sources/MemojiKit/MemojiKit.swift
+++ Sources/MemojiKit/MemojiKit.swift
@@ -1,12 +1,19 @@
 import SwiftUI
 
 public struct MemojiView: UIViewRepresentable {
-    public init() {}
+    
+    let color: UIColor?
+    
+    public init(color: Color?) {
+        self.color = UIColor(color ?? Color.secondary.opacity(0.2))
+    }
     
     public func makeUIView(context: Context) -> some UIView {
-        return UIMemojiView()
+        let view = UIMemojiView()
+        view.backgroundColor = color
+        
+        return view
     }
     
-    public func updateUIView(_ uiView: UIViewType, context: Context) {
-    }
+    public func updateUIView(_ uiView: UIViewType, context: Context) {}
 }
